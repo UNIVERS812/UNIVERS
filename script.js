74da522f8be3f801021c5ba299803e0cecefe4f3
UNIVERS812 <saidylkonate@gmail.com>
2025-08-29 20:24:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const boutton_de_recherche =document.getElementById("id1");
+const zone_de_recherche =document.getElementById("zonederecherche");
+
+boutton_de_recherche.addEventListener("click",()=>{zone_de_recherche.classList.toggle("hidden");
+
+});
